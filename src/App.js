29730873e52e9be4f289707c3621d09876fd2e72
noku4111 <thandiwem@gmail.com>
2021-03-10 19:22:27 +0100

    test push

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, {useState} from 'react';
+import Translate from './components/Translate';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+import Route from './components/Route';
+import Header from './components/Header'
+
+const items = [
+    {title: 'What is React?',
+     content: "React is a front end JS library"}, 
+
+     {title: 'Why use React?',
+     content: "React is a favorite JS library among engineers"}, 
+
+     {title: 'How do you use React?',
+     content: "You use by creating Components"}
+    ] ;
+
+    const options = [
+        {
+           label: 'The Color Red',
+            value: 'red'        
+        },
+
+        {
+            label: 'The Color Green',
+             value: 'green'        
+         },
+         {
+            label: 'A Shade of Blue',
+             value: 'blue'        
+         }
+
+    ];
+
+
+const App = () => {
+const [selected,setSelected] = useState(options[0])
+    return (
+        <div>
+        <Header/>
+        <Route path="/">
+            <Accordion items={items}/>
+        </Route>
+        
+        <Route path="/list">
+            <Search/>
+        </Route>
+        <Route path="/dropdown">
+            <Dropdown
+                label="Select a color"
+                options={options}
+                selected={selected}
+                onSelectedChange={setSelected}
+            />
+        </Route>
+        <Route path="/translate">
+            <Translate/>
+        </Route>
+        </div>
+    )
+}
+export default  App;
